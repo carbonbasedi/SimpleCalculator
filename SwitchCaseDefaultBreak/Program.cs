ea43aeaa6fe526dc06cc4062cb548ff475ea1a0b
carbonbasedi <carbonbasedai@gmail.com>
2023-01-19 16:16:13 +0400

    Simple Calculator

---
+++ SwitchCaseDefaultBreak/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("--- Enter first operand ---");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine("--- Enter second operand ---");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+        Check: Console.WriteLine("--- Enter operation type ---");
+            var operation = (Console.ReadLine());
+
+            {
+                switch (operation)
+                {
+
+                    case "+":
+                        Console.WriteLine($"Result is {num1 + num2}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"Result is {num1 - num2}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"Result is {num1 * num2}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"Result is {num1 / num2}");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operator input try again");
+                        goto Check;
+
+                }
+            }
+        }
+    }
+}
